b44c30802ef136ac91cfd8f7aadc9fd7e15d64ae
hungsinh2k4 <monternsh@gmail.com>
2023-02-20 10:56:42 +0700

    add 3

--- AdvProg_L1-GuessIt/guessit.cpp
+++ AdvProg_L1-GuessIt/guessit.cpp
@@ -100,7 +100,7 @@ bool checkContinuePlaying(char isContinued) {
 char getPlayerOpinion() {
     // TODO: Ask the player about continue playing and return the player's choice
     char isContinued;
-    cout << "Continue Playing ?(Y/N)"\n;
+    cout << "Continue Playing ?(Y/N)\n";
     cin >> isContinued;
     return isContinued;
 }
